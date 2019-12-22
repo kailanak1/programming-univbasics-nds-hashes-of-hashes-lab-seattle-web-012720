d55107454c13fdff4b8f1399da0ac338d83ce584
Kailana Kahawaii <kailanakahawaii@gmail.com>
2019-12-22 19:22:37 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -44,6 +44,16 @@ def naming_system
   
  
 naming_system = {
+  label: "Kingdom",
+  sub_category: {
+    label: "Phylum",
+    sub_category: {
+      label: "Class"
+      sub_category: {
+        label: "Order"
+      }
+    }
+  }
  
  }
   
