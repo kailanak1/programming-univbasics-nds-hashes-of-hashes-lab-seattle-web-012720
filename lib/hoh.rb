6a87a3f9cce7c799d2cb2c5c937a0e5478b4a4a2
Kailana Kahawaii <kailanakahawaii@gmail.com>
2019-12-22 19:26:21 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -54,7 +54,11 @@ naming_system = {
         sub_category: {
           label: "Family",
           sub_category: {
-            label: "Genus"
+            label: "Genus",
+            sub_category: {
+              label: "Species",
+              sub_category: 
+            }
           }
         }
       }
