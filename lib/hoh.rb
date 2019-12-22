13cb34da594430253c19aed807659e8e1d29f694
Kailana Kahawaii <kailanakahawaii@gmail.com>
2019-12-22 19:23:39 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -48,10 +48,9 @@ naming_system = {
   sub_category: {
     label: "Phylum",
     sub_category: {
-      label: "Class"
+      label: "Class",
       sub_category: {
         label: "Order"
-        }
       }
     }
   }
